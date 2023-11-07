e6437e7a2054ff662bc5f5db2fd78ffafb988457
Ulrich HOUNGBO <houngbou@gmail.com>
2023-11-07 21:29:29 +0100

    create class to manage basic auth

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -1,41 +1,34 @@
 #!/usr/bin/env python3
-""" Module to test the auth module"""
-
+""" Auth class, Require auth with stars """
 from flask import request
+from typing import List, TypeVar
 
-from typing import TypeVar
 
-
-class Auth:
-    """Auth class """
-
-    def __init__(self, username, password):
-        self.username = username
-        self.password = password
-
+class Auth():
+    """ manage the API authentication """
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
-        """Require auth function"""
-        if path is None or excluded_paths is None or excluded_paths == []:
+        """ require authorithation """
+        if path is None or excluded_paths is None or not len(excluded_paths):
             return True
         if path[-1] != '/':
             path += '/'
         for i in excluded_paths:
-            if path.endswith('*'):
-                if path.startswith(i):
+            if i.endswith('*'):
+                if path.startswith(i[:1]):
                     return False
-        if path not in excluded_paths:
+        if path in excluded_paths:
+            return False
+        else:
             return True
-        return False
 
     def authorization_header(self, request=None) -> str:
-        """Authorization header"""
+        """ authorization header """
         if request is None:
             return None
-        if not request.headers.get('Authorization'):
+        if not request.headers.get("Authorization"):
             return None
-        return request.headers.get('Authorization')
+        return request.headers.get("Authorization")
 
     def current_user(self, request=None) -> TypeVar('User'):
-        """Current user"""
+        """ current user """
         return None
-
