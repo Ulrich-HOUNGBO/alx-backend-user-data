3379f534213a96653236e80251e4309601ec128a
Ulrich HOUNGBO <houngbou@gmail.com>
2023-11-15 07:41:15 +0100

    hash password

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -7,7 +7,7 @@ hash password with bcrypt.hashpw
 import bcrypt
 
 
-def hash_password(password: str) -> str:
+def _hash_password(password: str) -> str:
     """ takes in a password string arguments and returns a string
         The returned string is a salted hash of the input password,
         hashed with bcrypt.hashpw """
