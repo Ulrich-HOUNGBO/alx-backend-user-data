a22b079886c8693ed5717ae7e0c004be2a43d064
Ulrich HOUNGBO <houngbou@gmail.com>
2023-11-15 13:46:42 +0100

    implement the end-point to register a user

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -3,6 +3,7 @@
     Get reset passwords token, Update password end-point """
 from flask import Flask, jsonify, request, abort, redirect
 from auth import Auth
+
 app = Flask(__name__)
 AUTH = Auth()
 
@@ -11,3 +12,15 @@ AUTH = Auth()
 def welcome():
     """ Basic Flask app, return a JSON """
     return jsonify({"message": "Bienvenue"})
+
+
+@app.route('/users', methods=['POST'], strict_slashes=False)
+def users():
+    """ Register user, return a JSON """
+    email = request.form.get('email')
+    password = request.form.get('password')
+    try:
+        AUTH.register_user(email, password)
+        return jsonify({"email": email, "message": "user created"}), 200
+    except ValueError:
+        return jsonify({"message": "email already registered"}), 400
